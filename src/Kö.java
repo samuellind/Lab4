71e418bcfe26c546a3fcef17dd194af79aaf48c6
Amar <amc0kru@gmail.com>
2013-05-07 22:44:50 +0200

    Revert "v.1.1"
    This reverts commit 4d97c54d6a401786932a9004054496473d3d7dd9.

--- src/Kö.java
+++ src/Kö.java
@@ -5,7 +5,9 @@ import javax.swing.JOptionPane;
 public class Kö {
 
 	private int _intAlder;
+	private boolean _checkAlder=false;
 	private JOptionPane _errorAlder;
+	private Person _newPerson;
 	private DefaultListModel _allaKunder, _listKo1, _listKo2;
 	
 	public Kö(DefaultListModel defAlla, DefaultListModel defKo1, DefaultListModel defKo2){
@@ -17,11 +19,13 @@ public class Kö {
 	public void createPerson(String fNamn, String eNamn, String alder){
 		_allaKunder.addElement(new Person(fNamn,eNamn,_intAlder));
 		System.out.println("Listitem: " + _allaKunder.elementAt(0));
+		//_newPerson = new Person(fNamn,eNamn,_intAlder);
 	}
 	
 	public boolean checkAlder(String alder){
 		try{
 			_intAlder = Integer.parseInt(alder);
+			_checkAlder=true;
 			return true;
 		}
 		catch(Exception f){
@@ -32,6 +36,14 @@ public class Kö {
 					JOptionPane.ERROR_MESSAGE);
 			return false;
 		}
+		/*catch(NumberFormatException e){
+			_errorAlder = new JOptionPane();
+			JOptionPane.showMessageDialog(_errorAlder,
+					"Ålder måste vara i siffror.",
+					"Inmatningsfel",
+					JOptionPane.ERROR_MESSAGE);
+			return false;
+		}*/
 	}
 	
 	public void moveToQueue1(Object selObj){
